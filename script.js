70a710616a5cd75f32fedb31c808dabb7415ca8d
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-12-16 23:22:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function toFahrenheit(celsius) {
  // Write your code here
+	const a = celsius+32;
+	return a.toFixed(2);
 }
 
-// Do not change the code below
+ // Do not change the code below
 const celsius = prompt("Enter Celsius:");
 alert(toFahrenheit(Number(celsius)));
